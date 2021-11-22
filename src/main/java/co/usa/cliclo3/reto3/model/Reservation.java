e38927af7d1b4a4b11942274bb1377bbff4c2d1d
Javii-dev <69944736+Javii-dev@users.noreply.github.com>
2021-11-22 13:40:27 -0500

    Se ajusta Reservation

--- src/main/java/co/usa/cliclo3/reto3/model/Reservation.java
+++ src/main/java/co/usa/cliclo3/reto3/model/Reservation.java
@@ -1,12 +1,19 @@
 package co.usa.cliclo3.reto3.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.aspectj.bridge.Message;
-
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
 
 @Entity
 @Table(name="reservations")
@@ -90,5 +97,5 @@ public class Reservation implements Serializable {
         this.score = score;
     }
 
-    
+   
 }
