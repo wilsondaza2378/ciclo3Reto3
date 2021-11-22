bc68733f7c85afbc20012e121f44086c0d0be30e
Javii-dev <69944736+Javii-dev@users.noreply.github.com>
2021-11-22 14:57:13 -0500

    Se ajusta resrvation message

--- src/main/java/co/usa/cliclo3/reto3/model/Message.java
+++ src/main/java/co/usa/cliclo3/reto3/model/Message.java
@@ -56,5 +56,5 @@ public class Message implements Serializable {
         this.client = client;
     }
 
-
+ 
 }
--- src/main/java/co/usa/cliclo3/reto3/model/Reservation.java
+++ src/main/java/co/usa/cliclo3/reto3/model/Reservation.java
@@ -1,20 +1,12 @@
 package co.usa.cliclo3.reto3.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.aspectj.bridge.Message;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
-
+import java.util.List;
 
 @Entity
 @Table(name="reservations")
@@ -98,5 +90,5 @@ public class Reservation implements Serializable {
         this.score = score;
     }
 
-  
+     
 }
